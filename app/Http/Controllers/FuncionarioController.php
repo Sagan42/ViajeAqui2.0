b3b0f92192ba50decc656225aaf64d78d2e186fd
danielsantana77 <daniel_santana_oliveira@hotmail.com>
2021-12-08 02:09:30 -0300

    Tela Vender Passagens Funcionário

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\ClienteController;
 use App\Models\Funcionario;
+use App\Models\Linha;
+use App\Models\Agenda;
+use Illuminate\Support\Carbon;
 
 class FuncionarioController extends Controller
 {
@@ -88,6 +91,98 @@ class FuncionarioController extends Controller
         Funcionario::destroy($id);
     }
     
+    public function selecionarPassagens(Request $request){
 
+       
+        $linhaPesq = new Linha;
+        $linhaPesq->origem = $request->SelecionarOrigem;
+        $linhaPesq->destino = $request->SelecionarDestino;
+        $dataSaida = $request->dataSaida;
+        $linhaPesq->dataSaida = $dataSaida;
+        //2021-12-01 -> quarta
+        // 1 -> quinta
+        // 8
+        // 2 -> sexta
+        // 9
+
+        $agenda = Agenda::all();
+        $linha = Linha::all();
+        $linhas = array();
+        $agendas = array();
+        
+        // Data de hoje
+        //$dia = Carbon::today()->format('l');
+        date_default_timezone_set('America/Sao_Paulo');
+        //$dia = Carbon::createFromFormat('Y-m-d', $dataSaida)->toDateString();
+
+        
+        //$dataPesquisado = Carbon::createFromFormat('Y-m-d', $dataSaida);
+        $dataPesquisado = Carbon::createFromFormat('Y-m-d', $dataSaida)->format('d/m/Y');
+        
+        //$dataPesquisado->add(1,'day');
+        //$weekdays = Carbon::getDays();
+        //$diaSemanaPesquisado = Carbon::create($dataPesquisado)->locale('pt-BR')->dayName;
+
+        //dd($diaSemana);
+        //retorna data com formato carbon
+        //$test = Carbon::createFromFormat('Y-m-d', $dataSaida)->toDateString();
+
+        $diaSemanaPesquisado = Carbon::create($dataSaida)->locale('pt-BR')->dayName;
+
+        foreach($agenda as $a){
+            if($a->dia_semana == $diaSemanaPesquisado) {
+                $linhaAux = Linha::find($a->id_linha);
+                if($linhaAux->tipoLinha == 'Direta'){
+                    if($linhaAux->origem == $request->SelecionarOrigem && $linhaAux->destino == $request->SelecionarDestino){
+                        array_push($linhas,$linhaAux);
+                    }
+                    
+                }else{
+                    $linhaPesquisada = $linhaAux;
+                    $linhaPesquisada->preco = 0;
+                    $destino = $linhaAux->destino;
+    
+                    $preco = 0;
+    
+                    foreach($linha as $l){
+                        if($l->num_linha == $linhaAux->num_linha){
+                            if($l->origem == $request->SelecionarOrigem && $l->destino == $request->SelecionarDestino){
+                                $l->id = $linhaAux->id;
+                                array_push($linhas,$l);
+                            }elseif($linhaAux->origem == $request->SelecionarOrigem){
+                                $preco += $l->preco;
+                                if($destino == $l->origem){
+                                    if($l->destino == $request->SelecionarDestino){
+                                        $linhaPesquisada->destino = $l->destino;
+                                        $linhaPesquisada->preco = $preco;
+                                        array_push($linhas,$linhaPesquisada);
+                                    }
+                                    $destino = $l->destino;
+                                }
+                            }else{
+                                if($l->origem == $request->SelecionarOrigem){
+                                    $linha = $l;
+                                    $linha->id = $linhaAux->id;
+                                    $preco = 0;
+                                }
+    
+                                if($linha != null || $linha->origem == $request->SelecionarOrigem){
+                                    $preco += $l->preco;
+                                    if($l->destino == $request->SelecionarDestino){
+                                        $linha->preco = $preco;
+                                        $linha->destino = $l->destino;
+                                        array_push($linhas,$linha);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return view('funcionario.venderPassagens', ['linha' => $linhas, 'agenda' => $agenda, 'linhaPesquisada'=> $linhaPesq, 'dia' => $diaSemanaPesquisado, 'dataSaida' => $dataPesquisado]);
+
+    }
     
 }
