774cf8a0a5a1a20a57c7605be5b6c38b4fdac658
Pegasus77-Adriel <adrielsantana7071@gmail.com>
2021-12-08 12:16:47 -0300

    Update cadUsuario.blade.php

--- resources/views/adm/cadUsuario.blade.php
+++ resources/views/adm/cadUsuario.blade.php
@@ -8,27 +8,27 @@
         
               <div>
                 <label for="" >Nome   </label>
-                <input type="text" name="cadNome"  class = "input-cadastrar-usuario"> 
+                <input type="text" name="cadNome"  class = "input-cadastrar-usuario form-control @error('cadNome')is-invalid @endif"  value="{{old('cadNome')}}"> 
               </div>
               
               <div>
                 <label for="">Senha</label>
-                <input type="password" name="cadSenha" class = "input-cadastrar-usuario"> 
+                <input type="password" name="cadSenha" class = "input-cadastrar-usuario form-control @error('cadSenha')is-invalid @endif"  value="{{old('cadSenha')}}"> 
               </div>
 
              <div>
                 <label for="">CPF</label>
-                <input type="text" name="cadCPF" class = "input-cadastrar-usuario"> 
+                <input type="text" name="cadCPF" class = "input-cadastrar-usuario form-control @error('cadCPF')is-invalid @endif"  value="{{old('cadCPF')}}"> 
              </div>
 
               <div>
                 <label for="">Celular</label>
-                <input type="text" name="cadCelular" class = "input-cadastrar-usuario"> 
+                <input type="text" name="cadCelular" class = "input-cadastrar-usuario form-control @error('cadCelular')is-invalid @endif"  value="{{old('cadCelular')}}"> 
               </div>
               
               <div>
                 <label for="">Email</label>
-                <input type="text" name="cadEmail" class = "input-cadastrar-usuario"> 
+                <input type="text" name="cadEmail" class = "input-cadastrar-usuario form-control @error('cadEmail')is-invalid @endif"  value="{{old('cadEmail')}}"> 
               </div>
               
               <div class="div-permissao">
